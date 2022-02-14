b4a20848d20a10b5dc0db69b2001cd0a23a6f6f9
Yuta Aoki <nightrage.metal66@gmail.com>
2022-02-14 22:10:08 +0900

    Seperated docker files into local and prod

--- sh/laravel/laravel.local.sh
+++ sh/laravel/laravel.local.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# initialize laravel
+cd /var/www/src
+composer install --optimize-autoloader
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
 # Run Laravel migration (by force, since it would be a prod-environment)
-php /var/www/src/artisan migrate --force
+php artisan migrate
 
 # Run Apache in "foreground" mode (the default mode that runs in Docker)
 apache2-foreground
