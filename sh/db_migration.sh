8d1472751af43ef4ca299599a0904005ca7a34e3
Yuta Aoki <nightrage.metal66@gmail.com>
2022-02-14 15:02:18 +0900

    Fixed db_migration.sh

--- sh/db_migration.sh
+++ sh/db_migration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run Laravel migration (by force, since it would be a prod-environment)
-php ../src/artisan migrate --force
+php /var/www/src/artisan migrate --force
 
 # Run Apache in "foreground" mode (the default mode that runs in Docker)
 apache2-foreground
